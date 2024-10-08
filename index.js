72bd6c3229552c00707554254aff79cabcb7ebc3
Ashutosh Kumar <ashutosh.kumar@smartfoodsafe.com>
2024-10-08 13:25:41 +0530

    ic

---
+++ index.js
@@ -0,0 +1,32 @@
+//https://www.npmjs.com/package/http-proxy-middleware?activeTab=readme#http-proxy-middleware-options
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+
+let PORT = 5000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(
+  "/*",
+  createProxyMiddleware({
+    target: "ws://localhost:5050/my/socket",
+    ws: true,
+    changeOrigin: true,
+  })
+);
+
+app.listen(PORT, () => {
+  const message = `\nInfo: Application started running\n-----------------------------\n \nPort: ${PORT}\nTime: ${new Date()}\n-----------------------------`;
+  console.info(message);
+});
+
+process.on("unhandledRejection", (reason, p) =>
+  console.error(reason, "Proxy Server - Unhandled Rejection at Promise", p)
+);
+
+process.on("uncaughtException", (err) =>
+  console.error(err, "Proxy Server - Uncaught Exception thrown")
+);
